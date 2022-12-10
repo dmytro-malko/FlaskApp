9b21bcc571143f6d8eb6205ab77e2bf206236be2
malkodmitriy <malko_dmitriy@ukr.net>
2022-12-10 10:31:18 +0200

    Create FlaskApp

---
+++ main.py
@@ -0,0 +1,4 @@
+import tecdoc
+
+if __name__=='__main__':
+    tecdoc.app.run(debug=True, port=4567)
---
+++ tecdoc/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+
+
+import yaml
+
+app = Flask(__name__)
+Bootstrap(app)
+# DB configuration
+db = yaml.load(open('db.yaml'), Loader=yaml.FullLoader)
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+from tecdoc import ruotes, model
